5ace761def0511439458597a4339d19d1f211878
sergio3412 <32843676+sergio3412@users.noreply.github.com>
2021-09-10 15:28:23 -0300

    Add files via upload

--- js/reportAll.js
+++ js/reportAll.js
@@ -1,16 +1,10 @@
 
-var contador=0;
-var nomeGrupo=[];
-var idGrupo=[];
-var emailUsuario=[];
-
-
-
-
-var contadorGrupo=0;
-var contadorPorEmails=0;
-
-var controlImp=0;
+var nomeGrupo=[]; //salvar no array todos os nomes dos grupos
+var idGrupo=[];  //salvar no array os codigos dos grupos para buscar depois nos acordos
+var emailUsuario=[]; //salvar no array todos os e-mails dos usuarios
+var somaTotal=0;//total de transações dos grupos
+var contadorGrupo=0; //contador de transações por grupo
+var controlImp=0;//contador de transações por Emails
 
 
 function getUsers() {
@@ -35,6 +29,7 @@ return fetch('https://api.na4.echosign.com/api/rest/v6/users', {
 
     for(var i=0;i< response.userInfoList.length; i++){
       emailUsuario[i]=response.userInfoList[i].email;//listar emails e salvar no array
+        
     }
  
   })
@@ -46,12 +41,7 @@ return fetch('https://api.na4.echosign.com/api/rest/v6/users', {
 getUsers();
 
 
-
-
-
-
-
-
+/* FUNÇÃO PARA SALVAR NO ARRAY NOMES E CONDIGOS DOS GRUPOS */
 function getGroups() {
 const myHeaders = new Headers({
     'Content-Type': 'application/json',
@@ -75,6 +65,7 @@ return fetch('https://api.na4.echosign.com/api/rest/v6/groups', {
     for(var k=0;k< response.groupInfoList.length; k++){
       nomeGrupo[k]=response.groupInfoList[k].groupName;//listar nome de grupo e id e salvar no array
       idGrupo[k]=response.groupInfoList[k].groupId;
+      
     }
 
   })
@@ -86,7 +77,7 @@ return fetch('https://api.na4.echosign.com/api/rest/v6/groups', {
 getGroups();
 
 
-
+/* FUNÇÃO PARA BUSCAR GRUPOS E EMAILS NA LISTA DE ACORDOS*/
 
 function getAgreements() {
 const myHeaders = new Headers({
@@ -107,57 +98,65 @@ return fetch('https://api.na4.echosign.com/api/rest/v6/agreements', {
     }
   })        
   .then(response => {
-
+var html="";
     //IMPRIMIR GRUPOS
     for(var j=0;j<nomeGrupo.length;j++){//NOME GRUPO
-      var html = "<tr>";
+      
+       html = "<tr>";
          
           html +="<td class="+"coluna"+">"+nomeGrupo[j]+"</td>";
             
               //CONTAR ACORDOS POR GRUPO 
-              contador=0;
+              
+              contadorGrupo=0;//zerar o contadorGroup para contagem de outro grupo
               for(var z=0;z< response.userAgreementList.length; z++){
                 if(response.userAgreementList[z].groupId==idGrupo[j]){                    
                     contadorGrupo++;
                 }
               }//FIM CONTAR POR GRUPO
+              somaTotal=somaTotal+contadorGrupo;
               html +="<td class="+"coluna"+">"+contadorGrupo+" Acuerdos en el grupo</td>"; //adicionar valor contado por grupos
       
           html +="</tr>";//FIM NOME GRUPOS
 
         //IMPRIMIR EMAILS
-       controlImp=0;
+        controlImp=0;//zerar o controlImp para contagem de outro email de usuário na lista de acordos (segundo o grupo que pertence)
         for(var y=0;y<emailUsuario.length;y++){
           
-          var auxConteudo="";
+          var auxConteudo="";//Utilizado para não imprimir filas vazias 
           html +="</tr>";
           for(var x=0;x< response.userAgreementList.length; x++){
-           
-            if(response.userAgreementList[x].groupId==idGrupo[j]){
-              var emailAgr=response.userAgreementList[x].displayParticipantSetInfos[0].displayUserSetMemberInfos[0].email;
+
+            var emailAgr=response.userAgreementList[x].displayParticipantSetInfos[0].displayUserSetMemberInfos[0].email;
               
-                if(emailAgr==emailUsuario[y]){//contar emails por grupo
-                
+            if(emailAgr==emailUsuario[y]){//contar emails por grupo
+           
+                if(response.userAgreementList[x].groupId==idGrupo[j]){
                     controlImp++;
                     auxConteudo=emailAgr;
                   
                 }
 
             }
-                
+            
           }
           if(controlImp!=0 && auxConteudo!=""){
            html +="<td>"+auxConteudo+"</td>";
            html +="<td>"+controlImp+"</td>";
           }
-          
+           
         html +="</tr>";
         
-        }
+        } // FIM IMPRIMIR EMAILS
 
         $('table tbody').append(html);
-    }
-
+      }
+        // IMPRIMIR TOTAL DE ACORDOS POR GRUPO
+        html ="<tr>";
+        html +="<td>TOTAL</td>";
+        html +="<td>"+somaTotal+"</td>";
+        html +="</tr>";
+        $('table tbody').append(html);
   })
   .catch(error => {
     console.error(error);
